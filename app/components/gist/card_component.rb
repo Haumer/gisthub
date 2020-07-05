d77289cb79c72e1377f21fc71bbfe6d31cdae860
alexander <alexander.haumer@me.com>
2020-07-05 20:12:28 +0100

    edit gists link

--- app/components/gist/card_component.rb
+++ app/components/gist/card_component.rb
@@ -1,5 +1,6 @@
 class Gist::CardComponent < ViewComponent::Base
   def initialize(gist)
     @gist = gist
+    @edit_url = Github::Gists::Api.new(@gist.user).edit_gist_url(@gist)
   end
 end
--- app/services/github.rb
+++ app/services/github.rb
@@ -3,17 +3,20 @@ module Github
     class Api
       def initialize(user)
         @user = user
-        @gist_url = "https://api.github.com/users/#{@user.githubname}/gists".freeze
-        @data = call_api
+        @gist_url = "https://api.github.com/users/#{@user.githubname}".freeze
+      end
+
+      def edit_gist_url(gist)
+        "https://gist.github.com/#{gist.user}/#{gist.gist_id}/edit"
       end
 
       def call_api
-        uri = URI(@gist_url)
-        JSON.parse(Net::HTTP.get(uri))
+        uri = URI("#{@gist_url}/gists")
+        @data = JSON.parse(Net::HTTP.get(uri))
       end
 
       def save_gists
-        @data.each do |gist|
+        call_api.each do |gist|
           created_gist = UserGist.find_or_create_by(
             gist_id: gist["id"],
             date: gist["created_at"],
