dd6905ac3d79f185cbec2915f87eef44d8749971
alexander <alexander.haumer@me.com>
2020-07-07 00:46:45 +0100

    user_gist policy

--- app/policies/user_gist_policy.rb
+++ app/policies/user_gist_policy.rb
@@ -4,4 +4,24 @@ class UserGistPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    true
+  end
+
+  def index?
+    true
+  end
+
+  def create?
+    !user.nil?
+  end
+
+  def toggle_hide
+    record.user == current_user?
+  end
+
+  def toggle_star
+    record.user == current_user?
+  end
 end
