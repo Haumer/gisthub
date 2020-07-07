fa9e3c9642ba1db95dcdbe43c356f33273827526
alexander <alexander.haumer@me.com>
2020-07-07 23:30:13 +0100

    home page gist usage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
+if ENV['development']
+  Language.destroy_all
+end
+
 Language.create(name: "Ruby", img_url: "https://img.icons8.com/office/64/000000/ruby-programming-language.png")
 Language.create(name: "Javascript", img_url: "https://img.icons8.com/color/64/000000/javascript.png")
 Language.create(name: "Python", img_url: "https://img.icons8.com/color/64/000000/python.png")
 Language.create(name: "HTML", img_url: "https://img.icons8.com/color/64/000000/html-5.png")
 Language.create(name: "SCSS", img_url: "https://img.icons8.com/color/64/000000/css3.png")
 Language.create(name: "CSS", img_url: "https://img.icons8.com/color/64/000000/css3.png")
+Language.create(name: "Text", img_url: "https://img.icons8.com/ios-glyphs/64/000000/text.png")
+Language.create(name: "Markdown", img_url: "https://img.icons8.com/officel/64/000000/markdown.png")
