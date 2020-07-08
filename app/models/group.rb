d292736d6254ba44165eb0da9db86089791daaf5
alexander <alexander.haumer@me.com>
2020-07-09 00:03:18 +0100

    lengthende the randomness for set slug

--- app/models/group.rb
+++ app/models/group.rb
@@ -9,7 +9,7 @@ class Group < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
   validates :slug, length: { minimum: 2 }, format: { with: /.(\d+)./,
-    message: "only allows letters" }, on: :update, uniqueness: true
+    message: "need at least one letter and one digit" }, on: :update, uniqueness: true
 
   def slug_changed?
     !slug.blank?
@@ -17,13 +17,13 @@ class Group < ApplicationRecord
 
   def set_default_slug
     set_slug = ""
-    10.times { set_slug += (("a".."z").to_a + ("1".."9").to_a).sample }
+    6.times { set_slug += (("a".."z").to_a).sample }
+    6.times { set_slug += (("1".."9").to_a).sample }
+
     self.update(slug: set_slug)
-    # raise
   end
 
   def gists
-    # users.map(&:user_gists).flatten.reject(&:hide).sort_by(&:updated_at).reverse
     user_gists.reject(&:hide).sort_by(&:updated_at).reverse
   end
 
