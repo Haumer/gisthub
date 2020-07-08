d4d3bfc423d713ddbc8cfb485689823b150e94ce
alexander <alexander.haumer@me.com>
2020-07-08 21:08:42 +0100

    github links and usergroup

--- app/controllers/user_groups_controller.rb
+++
@@ -1,2 +0,0 @@
-class UserGroupsController < ApplicationController
-end
---
+++ app/controllers/usergroups_controller.rb
@@ -0,0 +1,20 @@
+class UsergroupsController < ApplicationController
+  def create
+    @group = Group.find(params[:group_id])
+    @usergroup = Usergroup.new(user: User.find(strong_params["user"].to_i))
+    @usergroup.group = @group
+    if @usergroup.save!
+      flash[:notice] = "Success"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:error] = "Something went wrong"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:usergroup).permit(:user)
+  end
+end
