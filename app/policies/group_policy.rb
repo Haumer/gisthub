aa8eb057267466f428905222c7107c406d30986e
alexander <alexander.haumer@me.com>
2020-07-09 11:33:19 +0100

    check if user is owner for group changes

--- app/policies/group_policy.rb
+++ app/policies/group_policy.rb
@@ -18,16 +18,21 @@ class GroupPolicy < ApplicationPolicy
   end
 
   def update?
-    user_is_owner
+    user_is_owner?
+  end
+
+  def logged_in_and_owner?
+    user_logged_in? && user_is_owner?
   end
 
   private
+
 
   def user_logged_in?
     !user.nil?
   end
 
-  def user_is_owner
+  def user_is_owner?
     record.user == user
   end
 end
