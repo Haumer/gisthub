a328382e8f6d797d3458e6f79678f0a4dfb639c4
alexander <alexander.haumer@me.com>
2020-07-15 22:10:42 +0100

    cant edit gists from profile (moved to My_gists page)

--- app/components/gist/card_component.rb
+++ app/components/gist/card_component.rb
@@ -1,4 +1,5 @@
 class Gist::CardComponent < ViewComponent::Base
+  include ApplicationHelper
   def initialize(gist:, user:)
     @gist = gist
     @edit_url = Github::Gists::Api.new(@gist.user).edit_gist_url(@gist)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def controller_is_users_show?(params)
+    params[:controller] == "users" && params[:action] == "show"
+  end
 end
