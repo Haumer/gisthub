e3a44442d6a86ccfa7680266988f07eb3852ef31
alexander <alexander.haumer@me.com>
2020-07-28 01:25:50 +0100

    added svg and revamped basic homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @users = User.joins(:user_gists).group('users.id').order("count(users.id) DESC").limit(10)
   end
 
   def howto
