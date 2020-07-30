411b97f46221f338522f466e35b3a4ec1de2671c
alexander <alexander.haumer@me.com>
2020-07-30 02:38:31 +0100

    added more stats

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,16 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @users = User.joins(:user_gists).group('users.id').order("count(users.id) DESC").limit(5)
+    @top_gist_users = User.joins(:user_gists).group('users.id').order("count(users.id) DESC").limit(5)
+    # FIXME (haumer): convert into sql query
+    top_lines_users = User.all.map do |user|
+      [user, [user.user_gists&.map { |gist| gist.gist_files.pluck(:size) }.sum].flatten.sum ]
+    end
+    @top_lines_users = top_lines_users.sort_by do |info|
+      -info.last
+    end.first(5)
+
+    @languages = GistFile.group(:language).count(:language).sort_by { |l, v| -v }.first(5)
   end
 
   def howto
