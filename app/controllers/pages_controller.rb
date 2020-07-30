62af817e08ff2775f06c3a136da7d710eebd7f22
alexander <alexander.haumer@me.com>
2020-07-30 23:55:43 +0100

    added spacer and fixed styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,10 @@ class PagesController < ApplicationController
   def home
     @top_gist_users = User.joins(:user_gists).group('users.id').order("count(users.id) DESC").limit(5)
     # FIXME (haumer): convert into sql query
-    top_lines_users = User.all.map do |user|
-      [user, [user.user_gists&.map { |gist| gist.gist_files.pluck(:size) }.sum].flatten.sum ]
-    end
-    @top_lines_users = top_lines_users.sort_by do |info|
-      -info.last
-    end.first(5)
+    @top_lines_users = User.all.map do |user|
+      [user, user.user_gists&.map { |gist| gist.gist_files.count }.sum ]
+    end.sort_by { |info| -info.last }.first(5)
+
 
     @languages = GistFile.group(:language).count(:language).sort_by { |l, v| -v }.first(5)
   end
