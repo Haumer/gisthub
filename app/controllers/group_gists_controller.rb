29d22244e69aa3fa1c63e87aa1c59bfaf5978c43
alexander <alexander.haumer@me.com>
2020-08-09 16:51:10 +0100

    groupgist form

--- app/controllers/group_gists_controller.rb
+++ app/controllers/group_gists_controller.rb
@@ -1,2 +1,13 @@
 class GroupGistsController < ApplicationController
+  def create
+    @group_gist = GroupGist.new(group: find_group, user: current_user)
+    @group_gist.save!
+    raise
+  end
+
+  private
+
+  def find_group
+    Group.find(params.require("group_gist").permit(:group)[:group].to_i)
+  end
 end
