f464d8fc3cc8ad706db3a8528f29c65e393a1251
alexander <alexander.haumer@me.com>
2020-09-21 01:22:01 +0100

    removed change to devise tokens

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,12 +271,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  raise
-  if Rails.env.development?
-    config.omniauth :github, ENV['DEVE_APP_ID'], ENV['DEV_APP_SECRET'], scope: 'user,public_repo'
-  else
-    config.omniauth :github, ENV['APP_ID'], ENV['APP_SECRET'], scope: 'user,public_repo'
-  end
+  config.omniauth :github, ENV['APP_ID'], ENV['APP_SECRET'], scope: 'user,public_repo'
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
