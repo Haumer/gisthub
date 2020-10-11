707a4e71cf5cf63e80efe9d482c6c456cf9f2003
alexander <alexander.haumer@me.com>
2020-10-11 18:54:59 +0100

    fix typo

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,4 @@
 class Language < ApplicationRecord
-  has_many :gist_file_languages, depentent: :destroy
+  has_many :gist_file_languages, dependent: :destroy
   has_many :gist_files, through: :gist_file_languages
 end
