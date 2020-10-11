159838a8fbf42e964b144cb4dd3ae69765048db3
alexander <alexander.haumer@me.com>
2020-10-11 19:30:49 +0100

    changed from one to many

--- app/models/gist_file.rb
+++ app/models/gist_file.rb
@@ -1,5 +1,5 @@
 class GistFile < ApplicationRecord
   belongs_to :user_gist
-  has_one :gist_file_language, dependent: :destroy
+  has_many :gist_file_language, dependent: :destroy
   has_one :language, through: :gist_file_language
 end
