298ff4bd6051994eb9861600602e8fd5502849aa
alexander <alexander.haumer@me.com>
2020-10-21 23:30:56 +0100

    added StimulusJS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,26 +8,16 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-
-// ----------------------------------------------------
-// Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
-// WRITE YOUR OWN JS STARTING FROM HERE 👇
-// ----------------------------------------------------
-
 // External imports
 import "bootstrap";
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
 
+// Internal imports:
 document.addEventListener('turbolinks:load', () => {
 
 });
@@ -36,19 +26,3 @@ import { search, filter } from './groupSearch.js'
 import { invertColor } from './invertColor.js'
 import { collapseGists } from './collapseGists.js'
 import "controllers"
-
-// let cards = document.querySelectorAll(".gist-card")
-// Array.from(cards).forEach(card => {
-//   let option = card.querySelector(".options")
-//   let modal = document.querySelectorAll(".gist-options")
-//   modal.id = `Gist${card.dataset.gist}`
-
-//   card.addEventListener("click", () => {
-//     console.log(modal.id)
-//   })
-// })
-
-
-
-
-
