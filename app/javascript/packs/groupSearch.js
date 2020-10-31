251ff703c3e5f064e0ddf74f1cdbc7a7117a424e
alexander <alexander.haumer@me.com>
2020-10-31 22:40:07 +0000

    only set attr if form

--- app/javascript/packs/groupSearch.js
+++ app/javascript/packs/groupSearch.js
@@ -1,6 +1,9 @@
 let input = document.querySelector('#group_search_keyword')
 let results = Array.from(document.querySelectorAll('.group-results li'))
-const form = document.querySelector('.simple_form.group_search').setAttribute('autocomplete', 'off')
+const form = document.querySelector('.simple_form.group_search')
+if (form) {
+  form.setAttribute('autocomplete', 'off')
+}
 
 const search = (e) => {
   let keyword = e.target.value.trim()
