b7c088ae684914967d7f955bf5424dd117edbed9
alexander <alexander.haumer@me.com>
2020-11-06 18:02:52 +0000

    added rouge and made separate component for raw_code

--- app/components/gist/files_component.rb
+++ app/components/gist/files_component.rb
@@ -1,6 +1,14 @@
 class Gist::FilesComponent < ViewComponent::Base
+  require 'rouge'
+
   def initialize(gist:)
     @gist = gist
+  end
+
+  def highlighter(code)
+    formatter = Rouge::Formatters::HTML.new(css_class: 'highlight')
+    lexer = Rouge::Lexer.find('ruby')
+    formatter.format(lexer.lex(code))
   end
 
   attr_reader :gist
