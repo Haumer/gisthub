a5daca8e9d2b82f6ac5b4f91e20dc9dbf1dfb979
alexander <alexander.haumer@me.com>
2020-11-15 00:27:00 +0000

    initial coloring works

--- app/components/users/activity_component.rb
+++ app/components/users/activity_component.rb
@@ -12,6 +12,18 @@ class Users::ActivityComponent < ViewComponent::Base
     end
   end
 
+  def color_class(count)
+    if count >= 1 && count <= 5
+      "low-activity"
+    elsif count >= 6 && count <= 10
+      "medium-activiy"
+    elsif count >= 11
+      "high-activity"
+    else
+      "no-activity"
+    end
+  end
+
   private
 
   attr_reader :user, :calendar
