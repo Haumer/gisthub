c38979bc58b1669f271315165f969c5600ddc3b5
alexander <alexander.haumer@me.com>
2021-01-30 20:52:35 +0000

    reworking hom view

--- app/components/home/usage_component.rb
+++ app/components/home/usage_component.rb
@@ -1,6 +1,16 @@
 class Home::UsageComponent < ViewComponent::Base
-  def initialize(title:, long:)
+  def initialize(title:, long:, defunkt: false, gh: false)
     @title = title
     @long = long
+    @defunkt = defunkt
+    @gh = gh
   end
+
+  def render?
+    @defunkt || @gh
+  end
+
+  private
+
+  attr_reader :defunkt, :gh
 end
